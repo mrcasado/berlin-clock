a2a957e309e882640aeaad06a2441eece93dce99
Alex <acasadovidal@gmail.com>
2016-10-23 20:22:52 +0100

    Remove unused code

--- src/main/java/org/acv/berlin_clock/BerlinClockApp.java
+++ src/main/java/org/acv/berlin_clock/BerlinClockApp.java
@@ -1,14 +1,10 @@
 package org.acv.berlin_clock;
 
-import java.util.Scanner;
-
 public class BerlinClockApp {
 
-	public static void main(String[] args) {		
-//    Scanner scanner = new Scanner(System.in);
-//    String twentyFourHoursTime = scanner.nextLine();	
-	BerlinClock berlinClock = new BerlinClock();
-	System.out.println(berlinClock.convertTime(args[0]));
+	public static void main(String[] args) {
+		BerlinClock berlinClock = new BerlinClock();
+		System.out.println(berlinClock.convertTime(args[0]));
 	}
 
 }
